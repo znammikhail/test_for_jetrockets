26f0ca925ef7c4062fc7cdf22f6bc9c0810fd320
znammikhail <znamenmihail@gmail.com>
2024-02-15 08:45:23 +0300

    Added project

---
+++ users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import redirect, render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Поздравляем! Вы успешно зарегестрировались.")
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
